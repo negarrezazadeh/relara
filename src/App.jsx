d9302cc5d156e6a64616c89f7205804d445d10f8
rznegrr <negarrz1378@gmail.com>
2025-01-05 21:06:55 +0330

    📚 docs: install reactQuery devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import AppLayout from "./layouts/AppLayout";
@@ -37,6 +38,7 @@ function App() {
   return (
     <BrowserRouter future={{ v7_relativeSplatPath: true }}>
       <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
         <AuthContextProvider>
           <Toaster position="top-center" reverseOrder={false} />
           <Routes>
