13ad92c6da40eeae3ec3c630f87303155eb9e26e
rznegrr <negarrz1378@gmail.com>
2025-01-13 13:38:29 +0330

    🎨 design: change card style

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ children }) {
   return (
-    <div className="rounded-sm border border-gray-600 bg-dark-800 p-5 text-gray-200">
+    <div className="rounded-sm border border-gray-600 bg-dark-800 p-5 text-gray-200 max-h-[calc(100vh-170px)] overflow-auto">
       {children}
     </div>
   );
